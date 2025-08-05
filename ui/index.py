53035c4894da8da4b31a806f8851fdf978156dbd
Garv Thakral <garvthakralfx@gmail.com>
2025-08-05 13:54:27 +0530

    UI

---
+++ ui/index.py
@@ -0,0 +1,55 @@
+# import streamlit as st
+# import mlflow
+from transformers import TFDistilBertForSequenceClassification
+from transformers import AutoTokenizer
+import tensorflow as tf
+import datasets as ds
+labels = [
+    'admiration', 'amusement', 'anger', 'annoyance', 'approval', 'caring',
+    'confusion', 'curiosity', 'desire', 'disappointment', 'disapproval',
+    'disgust', 'embarrassment', 'excitement', 'fear', 'gratitude', 'grief',
+    'joy', 'love', 'nervousness', 'optimism', 'pride', 'realization', 'relief',
+    'remorse', 'sadness', 'surprise', 'neutral'
+]
+model:TFDistilBertForSequenceClassification = TFDistilBertForSequenceClassification.from_pretrained(
+    "./saved_models/trained_model",
+    num_labels=28,
+    problem_type="multi_label_classification"
+)
+model_name = "distilbert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+def tokenize_data(example):
+    example = tokenizer(example,padding = "max_length",truncation=True,max_length = 100)
+    print(example)
+    return dict(example)
+def preprocess_data(exampleList):
+    tokenized_example = [tokenize_data(example) for example in exampleList] 
+    return tokenized_example
+
+model.compile(
+    optimizer="adam",
+    loss=tf.keras.losses.BinaryCrossentropy(from_logits=True)
+,
+    metrics=["binary_accuracy", "AUC", "Precision", "Recall"]
+)
+text = ['I love you' , 'you are the best' , 'ew bro get off of me']
+text = preprocess_data(text)
+datasets_ds = ds.Dataset.from_list(text)
+tf_ds = datasets_ds.to_tf_dataset(
+    columns = ['input_ids','attention_mask'],
+    batch_size=8,
+    shuffle=True
+)
+pred = model.predict(tf_ds)
+print(pred.logits)
+threshold = 0.6
+logits_tensor = tf.convert_to_tensor(pred.logits)
+probs = tf.sigmoid(logits_tensor)
+updatedProbs = (probs > threshold).numpy().astype(int)
+
+for indx,i in enumerate(text):
+    print(i)
+    for idx,y in enumerate(updatedProbs[indx]):
+        if(y == 1):
+            print(labels[idx])
+# print(updatedProbs)
